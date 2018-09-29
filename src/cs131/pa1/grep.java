01e9af6d93979f066b518cecfadd18b6e86d12c6
Osamah Mandawi <32464574+Mandawi@users.noreply.github.com>
2018-09-29 16:47:18 -0400

    Update grep.java

--- src/cs131/pa1/grep.java
+++ src/cs131/pa1/grep.java
@@ -8,19 +8,23 @@ import java.util.Scanner;
 import cs131.pa1.filter.Message;
 
 public class grep extends SequentialFilter {
+	String line;
+	public void SequentialFilter(String line){
+		this.line=line;
+	}
 	@Override
-	protected String processLine(String line) {
-		output = new LinkedList<String>(); 
+	public void process(){
+		output = new LinkedList<String>();
 		String parts[] = line.trim().split(" ");
-		if (parts.length <= 1) { //( grep ) alone does not work
+		if (parts.length <= 1) { // ( grep ) alone does not work
 			System.out.print(Message.REQUIRES_PARAMETER.with_parameter(line));
 		} else if (parts.length == 2) {
 			if (input.isEmpty()) {
 				System.out.print(Message.REQUIRES_INPUT.with_parameter(line));
 			} else {
 				/*
-				 * separates into lines and find the line in the input that 
-				 * contains what we want
+				 * separates into lines and find the line in the input that contains what we
+				 * want
 				 */
 				while (!input.isEmpty()) {
 					String[] found = input.poll().split("\r\n");
@@ -31,26 +35,25 @@ public class grep extends SequentialFilter {
 					}
 				}
 			}
-		} else { 
+		} else {
 			/*
 			 * Can use this if ( grep a hello_world.txt ) is an allowable command
 			 */
-			/*cat display = new cat();
-			display.output = new LinkedList<String>();
-			display.processLine(parts[2].trim());
-			while (!display.output.isEmpty()) {
-				String[] found = display.output.poll().split("\r\n");
-				for (int i = 0; i < found.length; i++) {
-					if (found[i].contains(parts[1].trim())) {
-						output.add(found[i]);
-					}
-				}
-			}*/ 
+			/*
+			 * cat display = new cat(); display.output = new LinkedList<String>();
+			 * display.processLine(parts[2].trim()); while (!display.output.isEmpty()) {
+			 * String[] found = display.output.poll().split("\r\n"); for (int i = 0; i <
+			 * found.length; i++) { if (found[i].contains(parts[1].trim())) {
+			 * output.add(found[i]); } } }
+			 */
 			System.out.print(Message.INVALID_PARAMETER.with_parameter(line));
-			
+
 		}
-		return output.toString().substring(1, output.toString().length() - 1);
 	}
 
+	@Override
+	protected String processLine(String line) {
+		return null;
+	}
 
 }
