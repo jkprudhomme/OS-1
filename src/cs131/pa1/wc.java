fcd35a22c35ab37c86d9bd463c8d6994354511af
Osamah Mandawi <32464574+Mandawi@users.noreply.github.com>
2018-09-29 18:38:42 -0400

    Update wc.java

--- src/cs131/pa1/wc.java
+++ src/cs131/pa1/wc.java
@@ -22,13 +22,23 @@ public class wc extends SequentialFilter {
 				System.out.print(Message.REQUIRES_INPUT.with_parameter(line));
 			} else {
 				while (!input.isEmpty()) {
-					String[] countLines = input.poll().split("\r\n"); //WILL COUNT .txt, etc.
-					LC = countLines.length;
-					for (int i = 0; i < LC; i++) {
-						String[] countWords = countLines[i].split(" ");
-						WC += countWords.length;
-						for (int j = 0; i < WC; i++) {
-							CC += countWords[i].length();
+//					String[] countLines = input.poll().split("\n"); //WILL COUNT .txt, etc.
+//					LC = countLines.length;
+//					for (int i = 0; i < LC; i++) {
+//						String[] countWords = countLines[i].split(" ");
+//						WC += countWords.length;
+//						for (int j = 0; i < WC; i++) {
+//							CC += countWords[i].length();
+//						}
+//					}
+					String take = input.poll();
+					for (int i = 0; i < input.poll().length(); i++) {
+						if (take.charAt(i) == '\n') {
+							LC++;
+						} else if (take.charAt(i) == ' ') {
+							WC++;
+						} else {
+							CC++;
 						}
 					}
 				}
