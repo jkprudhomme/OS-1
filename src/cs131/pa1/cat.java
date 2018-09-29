316180592fbc072389ff0c58ac7232f9d436fc8b
Osamah Mandawi <32464574+Mandawi@users.noreply.github.com>
2018-09-29 17:39:20 -0400

    Update cat.java

--- src/cs131/pa1/cat.java
+++ src/cs131/pa1/cat.java
@@ -7,8 +7,12 @@ import java.util.Scanner;
 import cs131.pa1.filter.Message;
 
 public class cat extends SequentialFilter {
-	@Override
-	protected String processLine(String line) {
+	private String line;
+	public cat(String line) {
+		this.line=line;
+	}
+	
+	public void process() {
 		/*
 		 * create a new address/path using the given line.
 		 * 
@@ -28,7 +32,10 @@ public class cat extends SequentialFilter {
 		} else {
 			System.out.print(Message.FILE_NOT_FOUND.with_parameter("cat " + line));
 		}
-		return output.toString().substring(1, output.toString().length() - 1);
+	}
+	@Override
+	protected String processLine(String line) {
+		return line;
 	}
 
 }
