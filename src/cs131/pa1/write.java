777f5ab47a8ac511559545fcc9e789148996a1db
Jordan Prudhomme <jkprudhomme@brandeis.edu>
2018-09-29 17:08:28 -0400

    Update write.java

--- src/cs131/pa1/write.java
+++ src/cs131/pa1/write.java
@@ -12,19 +12,25 @@ import java.util.Scanner;
 import cs131.pa1.filter.Message;
 
 public class write extends SequentialFilter {
+	private String line;
+	
+	public write(String line){
+		this.line = line;
+	}
+	
 	@Override
-	protected String processLine(String line) {
+	public void process(){
 		/*
 		 * creates Path using line and writes using input
 		 */
 		Path togo = Paths.get(SequentialREPL.currentWorkingDirectory + "\\" + line.replaceAll("> ", "").trim());
 		String send = input.poll();
 		if (Files.exists(togo)) {
-			write(togo.toString(), send);
+			writeTo(togo.toString(), send);
 		} else {
 			create(togo.toString(), send);
 		}
-		return null; // no ouput
+//		return null; // no ouput
 	}
 
 	private static void create(String address, String content) {
@@ -38,6 +44,33 @@ public class write extends SequentialFilter {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		PrintWriter printWriter = new PrintWriter(writer);
+		printWriter.print(content);
+		printWriter.close();
+	}
+
+	private static void writeTo(String putter, String content) {
+		/*
+		 * for overwriting a file
+		 */
+		PrintWriter printWriter = null;
+		try {
+			printWriter = new PrintWriter(putter);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		printWriter.print(content);
+		printWriter.close();
+	}
+
+	@Override
+	protected String processLine(String line) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
 		PrintWriter printWriter = new PrintWriter(writer);
 		printWriter.print(content);
 		printWriter.close();
